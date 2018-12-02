d9048267def2d257796c447c01a648668c3d1e73
Kishore Dubagunta <kdubagunta@yahoo.com>
2018-12-01 20:16:06 -0600

    Fixed logic for average change

--- PyBank/main.py
+++ PyBank/main.py
@@ -15,8 +15,10 @@ with open(input_path, 'r') as input_file:
 
     # Holders to keep track of num months and total
     num_months = 0
-    total_change = 0
+    total = 0
     prev_month_pl = 0
+    first_month_pl = None
+    last_month_pl = 0
 
     # Holders to keep track of greatest increase
     greatest_increase_month = None
@@ -37,7 +39,12 @@ with open(input_path, 'r') as input_file:
         # monthly change
         change = profit_loss - prev_month_pl
         prev_month_pl = profit_loss
-        total_change += change
+        total += profit_loss
+
+        if first_month_pl is None:
+            first_month_pl = profit_loss
+
+        last_month_pl = profit_loss
 
         # Check if current month's P/L is more than current max
         if change > greatest_increase:
@@ -49,11 +56,15 @@ with open(input_path, 'r') as input_file:
             greatest_decrease = change
             greatest_decrease_month = month
     
+change_across_period = last_month_pl - first_month_pl
+num_changes = num_months - 1
+
+# Print Results
 print('Financial Analysis')
 print('----------------------------')
 print(f'Total Months: {num_months}')
-print(f'Total: ${total_change}')
-print(f'Average  Change: ${total_change/num_months:.2f}')
+print(f'Total: ${total:.0f}')
+print(f'Average  Change: ${change_across_period/num_changes:.2f}')
 print(f'Greatest Increase in Profits: {greatest_increase_month} (${greatest_increase})')
 print(f'Greatest Decrease in Profits: {greatest_decrease_month} (${greatest_decrease})')
 
@@ -61,8 +72,8 @@ with open('results.txt', 'w') as output_file:
     output_file.write('Financial Analysis\n')
     output_file.write('----------------------------\n')
     output_file.write(f'Total Months: {num_months}\n')
-    output_file.write(f'Total: ${total_change}\n')
-    output_file.write(f'Average  Change: ${total_change/num_months:.2f}\n')
+    output_file.write(f'Total: ${total:.0f}\n')
+    output_file.write(f'Average  Change: ${change_across_period/num_changes:.2f}\n')
     output_file.write(f'Greatest Increase in Profits: {greatest_increase_month} (${greatest_increase})\n')
     output_file.write(f'Greatest Decrease in Profits: {greatest_decrease_month} (${greatest_decrease})\n')
 
